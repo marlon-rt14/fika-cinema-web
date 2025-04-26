3737a2306c385e6b1c5ca237d51e7cdbf2995560
Marlon Ruiz <54191496+marlon-rt14@users.noreply.github.com>
2025-04-26 00:10:32 -0500

    chore: removed console

--- src/components/layouts/MovieLayout.tsx
+++ src/components/layouts/MovieLayout.tsx
@@ -3,7 +3,6 @@ import { Header } from "@components";
 import { Main } from "@components/layouts/components";
 
 export const MovieLayout = () => {
-  console.log("render MovieLayout");
   return (
     <>
       <Header />
